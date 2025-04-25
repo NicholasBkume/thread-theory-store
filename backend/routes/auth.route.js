4cfb8197f93b1cc054c20a465b09c5fc6b4c4c4d
NicholasBkume <burinicholas@gmail.com>
2025-04-24 19:55:36 -0500

    Update auth.route.js

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -4,11 +4,11 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
-router.post("/logout", logout);
-router.post("/refresh-token", refreshToken);
-router.get("/profile", protectRoute, getprofile);
+//router.post("/signup", signup);
+//router.post("/login", login);
+//router.post("/logout", logout);
+//router.post("/refresh-token", refreshToken);
+//router.get("/profile", protectRoute, getprofile);
 
 
 export default router;
