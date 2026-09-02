67e0cf0c96c5e382ee07dc1cb635f54971322f6e
NicholasBkume <burinicholas@gmail.com>
2026-09-02 12:02:31 -0500

    fix: handle Redis client errors safely

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -1,7 +1,14 @@
-import Redis from "ioredis"
+import Redis from "ioredis";
 import dotenv from "dotenv";
+import { logger } from "./logger.js";
 
 dotenv.config();
 
-export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
-// await redis.set("foo", "bar");
+export const redis = new Redis(process.env.UPSTASH_REDIS_URL, {
+    lazyConnect: true,
+    maxRetriesPerRequest: 1,
+});
+
+redis.on("error", (error) => {
+    logger.error("Redis client error", { error: error.message });
+});
