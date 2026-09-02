0468bed71176c5fde8529d5052b484b27b6b879f
NicholasBkume <burinicholas@gmail.com>
2026-09-02 13:34:14 -0500

    feat: add protected admin customer routes

--- backend/routes/account.route.js
+++ backend/routes/account.route.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { protectRoute } from "../middleware/auth.middleware.js";
-import { updateProfile, getWishlist, toggleWishlist, addAddress, deleteAddress, createReview, getProductReviews } from "../controllers/account.controller.js";
+import { protectRoute, adminRoute } from "../middleware/auth.middleware.js";
+import { updateProfile, getWishlist, toggleWishlist, addAddress, deleteAddress, createReview, getProductReviews, getCustomers, getCustomerById, resetCustomerPassword } from "../controllers/account.controller.js";
 
 const router = express.Router();
 router.use(protectRoute);
@@ -11,4 +11,7 @@ router.post("/addresses", addAddress);
 router.delete("/addresses/:addressId", deleteAddress);
 router.post("/reviews/:productId", createReview);
 router.get("/reviews/:productId", getProductReviews);
+router.get("/admin/customers", adminRoute, getCustomers);
+router.get("/admin/customers/:id", adminRoute, getCustomerById);
+router.post("/admin/customers/:id/reset-password", adminRoute, resetCustomerPassword);
 export default router;
